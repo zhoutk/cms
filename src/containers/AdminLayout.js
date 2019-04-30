38eae20ea6d90d3e9f8753938048a5e3675430f8
zhoutk <zhoutk@189.cn>
2019-04-30 14:14:03 +0800

    useState.

--- src/containers/AdminLayout.js
+++ src/containers/AdminLayout.js
@@ -1,48 +1,36 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import SideBar from '../components/SideBar'
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '../components/AppBar';
 import MainBoard from '../components/MainBoard'
+import { useState } from 'react'
+
+const PersistentDrawerLeft = ({ classes }) => {
+  const [open, setOpen] = useState(true)
+
+  const handleDrawerOpen = () => {
+    setOpen( true );
+  };
+
+  const handleDrawerClose = () => {
+    setOpen( false );
+  };
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar handleDrawerOpen = { handleDrawerOpen } open = { open } />
+      <SideBar handleDrawerClose = { handleDrawerClose } open = { open } />
+      <MainBoard open = { open } />
+    </div>
+  );
+}
 
 const styles = theme => ({
   root: {
     display: 'flex',
   }
 });
-
-class PersistentDrawerLeft extends React.Component {
-  state = {
-    open: true,
-  };
-
-  handleDrawerOpen = () => {
-    this.setState({ open: true });
-  };
-
-  handleDrawerClose = () => {
-    this.setState({ open: false });
-  };
-
-  render() {
-    const { classes } = this.props;
-    const { open } = this.state;
-
-    return (
-      <div className={classes.root}>
-        <CssBaseline />
-        <AppBar handleDrawerOpen = { this.handleDrawerOpen } open = { open } />
-        <SideBar handleDrawerClose = { this.handleDrawerClose } open = { open } />
-        <MainBoard open = { open } />
-      </div>
-    );
-  }
-}
-
-PersistentDrawerLeft.propTypes = {
-  classes: PropTypes.object.isRequired,
-  theme: PropTypes.object.isRequired,
-};
 
 export default withStyles(styles, { withTheme: true })(PersistentDrawerLeft);
